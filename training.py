dfbf6292f23465cccb1c34d87cfba7d5f4acf8d1
jaseyaws <jcartin@ykw.systems>
2023-02-25 14:43:26 -0500

    add a way to select random response in related tag

--- training.py
+++ training.py
@@ -41,3 +41,15 @@ for document in documents:
     bag = []
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower() for word in word_patterns)]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+        
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+    
+    random.shuffle(training)
+    training = np.array(training)
+    
+    train_x = list(training[:, 1])
+    train_y = list(training[:, 1])

f9f15ac8276545cc805d43b415700048ba2e3f03
jaseyaws <jcartin@ykw.systems>
2023-02-26 14:48:55 -0500

    add chatbot functionality

--- chatbot.py
+++ chatbot.py
@@ -21,7 +21,6 @@ def clean_up_sentence(sentence):
     return sentence_words
     
     
-
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
@@ -37,3 +36,27 @@ def predict_class(sentence):
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print("Bot is running!")
+
+while True:
+    message = input("")
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
+        

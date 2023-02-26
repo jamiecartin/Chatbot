edb5ad5a387c6057d47bd3fbe3da8ee6380bf10f
jaseyaws <jcartin@ykw.systems>
2023-02-26 14:09:21 -0500

    building neural networks

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.model')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)

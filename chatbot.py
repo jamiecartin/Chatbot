0b62cd7c5ef596ec1e93f68c3b089f2cf7260641
jaseyaws <jcartin@ykw.systems>
2023-02-26 14:33:29 -0500

    change defs to make more sense

--- chatbot.py
+++ chatbot.py
@@ -17,3 +17,23 @@ model = load_model('chatbot_model.model')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+    
+    
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]

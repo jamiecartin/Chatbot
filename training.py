04a31e9fc0733c7d2da6fe5c80fd12ac3f2a2f97
jaseyaws <jcartin@ykw.systems>
2023-02-26 17:57:50 -0500

    fix pickle.dump words to classes

--- training.py
+++ training.py
@@ -32,7 +32,7 @@ words = sorted(set(words))
 
 classes = sorted(set(words))
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
 output_empty = [0] * len(classes)
@@ -64,6 +64,6 @@ for document in documents:
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics='accuracy')
     
-    model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-    model.save('chatbox_model.model')
+    hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+    model.save('chatboxmodel.h5', hist)
     print("Done")
